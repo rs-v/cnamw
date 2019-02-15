fbdb9d41fcc933fc50e13c57421ae78d050532fa
rs-v <41462194+rs-v@users.noreply.github.com>
2019-02-15 19:31:03 +0800

    Update rd.js

--- rd.js
+++ rd.js
@@ -2,9 +2,10 @@ var ac="https://www.baidu.com/"  ;
 var  cs=document.getElementsByClassName("form-control")
 setTimeout("aco()","3000"); 
 alert("即将跳转")
-if(cs =null)
+if(cs ==null)
 {
 function   aco()
-{
-window.location.assign(ac+cs)}}
+  {
+window.location.assign(ac+cs)}
+}
 alert ("erro")
