e1da28535163ef3d278bbfd06b269fde2a6120fd
rs-v <41462194+rs-v@users.noreply.github.com>
2019-02-15 19:23:27 +0800

    0
    0

--- rd.js
+++ rd.js
@@ -2,6 +2,7 @@ var ac="https://www.baidu.com/"  ;
 var  cs=document.getElementsByClassName("form-control")
 setTimeout("aco()","3000"); 
 alert("即将跳转")
+if(cs!=  "null")
 function   aco()
 {
 window.location.assign(ac+cs)}
