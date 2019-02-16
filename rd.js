f90e3472c1a6faa10cf4f77ef2cb734c7caa55ea
rs-v <41462194+rs-v@users.noreply.github.com>
2019-02-16 13:07:01 +0800

    0
    0

--- rd.js
+++ rd.js
@@ -1,12 +1,20 @@
-var ac="https://www.baidu.com/"  ;
-var  cs=document.getElementById("dd").value
-//setTimeout("aco()","3000"); 
-alert("即将跳转")
-alert(cs)
-if(cs ==null)
-{
-function   aco()
-  {
+
+
+
+function   dg()  {
+	var ac="https://www.baidu.com/baidu?tn=monline_3_dg&ie=utf-8&wd="  ;
+	var  zz="<html>"
+	var zzx="</html>"
+  var  cs=document.getElementById("dd").value
+var ee=("https://i.loli.net/2019/02/16/5c67992bc4822.jpg")
+  function   aco(){
+ // setTimeout(kal,3000)
+  document.write(zz+"<body  background="+ee+">"+"<script>setTimeout(kal,3000); function kal(){ window.location.assign(\""+ac+cs+"\")} </script>"+"</body>"+zzx)   }
+function  kal(){
 window.location.assign(ac+cs)}
+if(cs !="")
+{aco()
 }
-alert ("erro")
+ else{ 
+ alert ("请输入内容")}
+}
