b74d62bbd5c3634fa37ec5ec2e0d7461d01614d8
rs-v <41462194+rs-v@users.noreply.github.com>
2019-02-16 13:49:23 +0800

    0
    0

--- rd.js
+++ rd.js
@@ -3,15 +3,16 @@
 
 function   dg()  {
 	var ac="https://www.baidu.com/baidu?tn=monline_3_dg&ie=utf-8&wd="  ;
-	var  zz="<html>"  
-	var zzx="</html>"   
+	var  zz="<html>"  ;
+	var zzx="</html>"  ; 
 	var cv="<meta name=\"viewport\" content=\"width=device-width,initial-scale=1\">"
-	var co =" <meta name=\"viewport\" content=\"width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no\">"
-  var  cs=document.getElementById("dd").value
+	var co =" <meta name=\"viewport\" content=\"width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no\">";
+
+  var  cs=document.getElementById("dd").value;
 var ee=("https://i.loli.net/2019/02/16/5c67992bc4822.jpg")
   function   aco(){
  // setTimeout(kal,3000)
-  document.write(zz+cv+co+"<body  background="+ee+">"+"<script>setTimeout(kal,3000); function kal(){ window.location.assign(\""+ac+cs+"\")} </script>"+"</body>"+zzx)   }
+  document.write(zz+cv+co+"<img src=\""+ee+"\"/>"+"<script>setTimeout(kal,3000); function kal(){ window.location.assign(\""+ac+cs+"\")} </script>"+"</body>"+zzx)   }
 function  kal(){
 window.location.assign(ac+cs)}
 if(cs !="")
