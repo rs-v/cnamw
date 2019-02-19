0ac25faba63e26a21dbe705e98dc086a7242c4c5
rs-v <41462194+rs-v@users.noreply.github.com>
2019-02-19 13:14:49 +0800

    0
    0

--- rd.js
+++ rd.js
@@ -9,7 +9,7 @@ function   dg()  {
 	var co =" <meta name=\"viewport\" content=\"width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no\">";
 
   var  cs=document.getElementById("dd").value;
-var ee=("https://i.loli.net/2019/02/16/5c67992bc4822.jpg")
+var ee=("https://i.loli.net/2019/02/19/5c6b9046949aa.jpg")
   function   aco(){
  // setTimeout(kal,3000)
   document.write(zz+cv+co+"<body background=\""+ee+"\""+"background-size:contain"+">"+"<script>setTimeout(kal,3000); function kal(){ window.location.assign(\""+ac+cs+"\")} </script>"+"</body>"+zzx)   }
@@ -19,5 +19,5 @@ if(cs !="")
 {aco()
 }
  else{ 
- alert ("请输入内容")}
+ alert ("请输入内容(｡･ω･｡)")}
 }
