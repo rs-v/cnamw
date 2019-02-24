e218481f4361a60bb1c3f8b6572fe1664f1590dd
rs-v <41462194+rs-v@users.noreply.github.com>
2019-02-24 20:13:42 +0800

    0
    0

---
+++ sd.js
@@ -0,0 +1,23 @@
+
+
+
+function   dg()  {
+	var ac=""  ;
+	var  zz="<html>"  ;
+	var zzx="</html>"  ; 
+	var cv="<meta name=\"viewport\" content=\"width=device-width,initial-scale=1\">"
+	var co =" <meta name=\"viewport\" content=\"width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no\">";
+         var cb="http://ding.fanqier.cn/f/vqcnnc"
+  var  cs=document.getElementById("dd").value;
+var ee=("https://i.loli.net/2019/02/19/5c6b9046949aa.jpg")
+  function   aco(){
+ // setTimeout(kal,3000)
+  document.write(zz+cv+co+"<body background=\""+ee+"\""+"background-size:contain"+">"+"<script>setTimeout(kal,3000); function kal(){ window.location.assign(\""+ac+cs+"\")} </script>"+"</body>"+zzx)   }
+function  kal(){
+window.location.assign(ac+cs)}
+if(cs !="52ni.ml")
+{aco()
+}
+ else{ 
+ alert ("请输入"52ni.ml"(｡･ω･｡)")}
+}
