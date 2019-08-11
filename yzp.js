be710a325a5187bfe2fd9d6cd60d28f38be3008b
13863049525 <w42023@outlook.com>
2019-08-11 14:01:45 +0800

    (>_<)auto

---
+++ yzp.js
@@ -0,0 +1,29 @@
+
+function setCookie(cname,cvalue,exdays){
+	var d = new Date();
+	d.setTime(d.getTime()+(exdays*24*60*60*1000));
+	var expires = "expires="+d.toGMTString();
+	document.cookie = cname+"="+cvalue+"p"+"; "+expires;
+}
+function  mai(){
+	a =window.location.search.length;
+	b=window.location.search.substr(1,a-1);
+	a3=window.atob(b);
+	a2=window.atob(a3)
+	var date=new Date()
+	   day=date.getDate();
+	   month = date.getMonth();
+	   mng  =month+1
+	   d=day+"";
+	   m=mng+"";
+	   enl=m+d;
+	
+	   if (a2==enl){
+		 
+		   setCookie(12,22,5);
+		   window.location.replace("../index.html");
+	   }
+	   else{document.write("<h1>Bad verify key 呵呵</h1>")}
+	
+	
+}
